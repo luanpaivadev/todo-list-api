040127dd73d216cafdce51184472e852c4b3513c
Luan Paiva <luanpaivadev@gmail.com>
2022-11-08 23:58:54 -0300

    Update TodoListApiApplication.java

--- src/main/java/com/luanpaiva/todolistapi/TodoListApiApplication.java
+++ src/main/java/com/luanpaiva/todolistapi/TodoListApiApplication.java
@@ -24,19 +24,4 @@ public class TodoListApiApplication {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(UserService userService) {
-
-        return args -> {
-            userService.saveRole(new Role(null, "ROLE_USER"));
-            userService.saveRole(new Role(null, "ROLE_ADMIN"));
-
-            userService.saveUser(new AppUser(null, "Luan Paiva", "luanpaivadev", "1234", new ArrayList<>()));
-            userService.saveUser(new AppUser(null, "Alane Paiva", "alanepaiva", "1234", new ArrayList<>()));
-
-            userService.addRoleToUser("luanpaivadev", "ROLE_ADMIN");
-            userService.addRoleToUser("alanepaiva", "ROLE_USER");
-        };
-    }
-
 }
