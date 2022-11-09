ed42c1b387e33392267c8f35a1cfa73bc72941f9
Luan Paiva <luanpaivadev@gmail.com>
2022-11-08 23:54:56 -0300

    Remoção de classes

--- src/main/java/com/luanpaiva/todolistapi/core/security/CustomAuthenticationFilter.java
+++ src/main/java/com/luanpaiva/todolistapi/core/security/CustomAuthenticationFilter.java
@@ -46,14 +46,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 30 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
 
         String refresh_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
