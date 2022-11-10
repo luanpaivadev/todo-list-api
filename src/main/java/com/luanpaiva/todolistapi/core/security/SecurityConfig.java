57eaa73fcd20c1d06b9424a4545b2c997f8bda56
Luan Paiva <luanpaivadev@gmail.com>
2022-11-10 18:27:38 -0300

    Update SecurityConfig.java

--- src/main/java/com/luanpaiva/todolistapi/core/security/SecurityConfig.java
+++ src/main/java/com/luanpaiva/todolistapi/core/security/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.luanpaiva.todolistapi.core.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +12,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
